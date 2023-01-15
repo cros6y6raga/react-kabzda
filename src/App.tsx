247c2741ed8734f337f9b19b6846fd7d67d2808a
cros6y6raga <crosbybraga@gmail.com>
2023-01-15 14:43:35 +0300

    test+

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,7 @@ function App() {
             {/*<Rating value={5}/>*/}
             {/*<Rating value={5}/>*/}
             {/*<Rating value={5}/>*/}
+            {/*<Rating value={5}/>*/}
         </div>
     );
 }
